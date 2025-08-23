55468d6a5d03cf6e5179856ac59f613cc43fef92
Onluiss <166739157+Onluiss@users.noreply.github.com>
2025-08-23 18:58:19 +0200

    Add card values constants for game

---
+++ constants/cardValues.js
@@ -0,0 +1,13 @@
+// constants/cardValues.js
+"use strict";
+
+export const cardValues = {
+  '1-c.png': 10, '2-c.png': 1, '3-c.png': 9, '4-c.png': 2, '5-c.png': 3,
+  '6-c.png': 4, '7-c.png': 5, '8-c.png': 6, '9-c.png': 7, '10-c.png': 8,
+  '1-o.png': 10, '2-o.png': 1, '3-o.png': 9, '4-o.png': 2, '5-o.png': 3,
+  '6-o.png': 4, '7-o.png': 5, '8-o.png': 6, '9-o.png': 7, '10-o.png': 8,
+  '1-s.png': 10, '2-s.png': 1, '3-s.png': 9, '4-s.png': 2, '5-s.png': 3,
+  '6-s.png': 4, '7-s.png': 5, '8-s.png': 6, '9-s.png': 7, '10-s.png': 8,
+  '1-m.png': 10, '2-m.png': 1, '3-m.png': 9, '4-m.png': 2, '5-m.png': 3,
+  '6-m.png': 4, '7-m.png': 5, '8-m.png': 6, '9-m.png': 7, '10-m.png': 8
+};

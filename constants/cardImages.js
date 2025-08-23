c6bb331c09472aff13846d13ee9966b1e3a5bb0f
Onluiss <166739157+Onluiss@users.noreply.github.com>
2025-08-23 18:56:59 +0200

    Add cardImages constant with card filenames

---
+++ constants/cardImages.js
@@ -0,0 +1,9 @@
+// constants/cardImages.js
+"use strict";
+
+export const cardImages = [
+  '1-c.png', '2-c.png', '3-c.png', '4-c.png', '5-c.png', '6-c.png', '7-c.png', '8-c.png', '9-c.png', '10-c.png',
+  '1-o.png', '2-o.png', '3-o.png', '4-o.png', '5-o.png', '6-o.png', '7-o.png', '8-o.png', '9-o.png', '10-o.png',
+  '1-s.png', '2-s.png', '3-s.png', '4-s.png', '5-s.png', '6-s.png', '7-s.png', '8-s.png', '9-s.png', '10-s.png',
+  '1-m.png', '2-m.png', '3-m.png', '4-m.png', '5-m.png', '6-m.png', '7-m.png', '8-m.png', '9-m.png', '10-m.png'
+];
